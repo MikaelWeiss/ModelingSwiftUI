43f03e2886ff7496026bc52d153a16e2f6c75ebd
Mikael Weiss <campingmikael@icloud.com>
2024-08-01 10:23:45 -0600

    Initial Commit

---
+++ ModelingSwiftUI/ModelingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelingSwiftUIApp.swift
+//  ModelingSwiftUI
+//
+//  Created by Mikael Weiss on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ModelingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
